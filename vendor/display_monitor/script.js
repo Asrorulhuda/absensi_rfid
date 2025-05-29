2ba56010565d4841dc0adb636fdbbd70c648f375
Asrorulhuda <asrorulhuda@gmail.com>
2025-05-29 15:02:26 +0700

    Add files via upload

---
+++ vendor/display_monitor/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.blog-slider__content', {
+      spaceBetween: 30,
+      effect: 'fade',
+      loop: true,
+      mousewheel: {
+        invert: false,
+      },
+      // autoHeight: true,
+      pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+      }
+    });
